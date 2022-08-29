e8b8e2ce883fb9cbca80e3050d9e75c798edb669
hal9000universe <benjamin.schoofs@aagcux.de>
2022-08-29 22:24:19 +0200

    [lunar-lander] slight optimizations

--- LunarLander/jax/lunar_lander.py
+++ LunarLander/jax/lunar_lander.py
@@ -1,19 +1,42 @@
+# py
+import os
+import time
+from statistics import mean
+from pickle import dump, load
+from typing import Mapping, Tuple, List
+
+# nn & rl
 import jax
 import jax.numpy as jnp
+import numba
 import numpy as np
 import haiku as hk
 import optax
 import gym
-import os
-
-import time
-from typing import Mapping, Tuple, List
-from numpy import ndarray, zeros, float64, int64, argmax, append, newaxis
 from jax.nn import one_hot
+from numpy import ndarray, zeros, float64, int64, argmax, append, newaxis
 from numpy.random import uniform, randint
 
-from statistics import mean
-from pickle import dump, load
+
+# TODO: implement Observation Wrapper
+class ObsWrapper(gym.Wrapper):
+
+    def __init__(self, environment):
+        super(ObsWrapper, self).__init__(environment)
+        self._step = 0
+
+    def _obs(self, observation: ndarray) -> ndarray:
+        fraction_finished: float = self._step / MAX_STEPS
+        return append(observation, fraction_finished)[newaxis, ...]
+
+    def step(self, action):
+        self._step += 1
+        observation, reward, done, info = self.env.step(action)
+        return self._obs(observation), reward, done, info
+
+    def reset(self, **kwargs):
+        self._step = 0
+        self.env.reset()
 
 
 class Model(hk.Module):
@@ -40,6 +63,8 @@ class Model(hk.Module):
         return Q
 
 
+# TODO: implement replay buffer structure from
+#  Fast Reinforcement Learning using online adjustments from the past
 class ReplayBuffer:
     _buffer_size: int
     _states: ndarray
@@ -66,6 +91,26 @@ class ReplayBuffer:
     def size(self) -> int:
         return self._samples
 
+    @property
+    def states(self) -> ndarray:
+        return self._states
+
+    @property
+    def actions(self) -> ndarray:
+        return self._actions
+
+    @property
+    def rewards(self) -> ndarray:
+        return self._rewards
+
+    @property
+    def observations(self) -> ndarray:
+        return self._observations
+
+    @property
+    def dones(self) -> ndarray:
+        return self._dones
+
     def add(self, state: ndarray, action: int, reward: float, observation: ndarray, done: bool):
         self._states[self._counter % self._buffer_size] = state
         self._actions[self._counter % self._buffer_size] = action
@@ -75,11 +120,14 @@ class ReplayBuffer:
         self._counter += 1
         self._samples = min(self._counter, self._buffer_size)
 
-    def sample_batch(self, batch_size: int = 64) -> Tuple[ndarray, ndarray, ndarray, ndarray, ndarray]:
-        random_indices: ndarray = randint(0, self._samples, batch_size)
-        batch = (self._states[random_indices], self._actions[random_indices], self._rewards[random_indices],
-                 self._observations[random_indices], self._dones[random_indices])
-        return batch
+
+@numba.njit
+def sample_batch(num_samples: int, states: ndarray, actions: ndarray, rewards: ndarray,
+                 observations: ndarray, dones: ndarray, batch_size: int = 64) -> Tuple[ndarray, ndarray, ndarray, ndarray, ndarray]:
+    random_indices: ndarray = randint(0, num_samples, batch_size)
+    batch = (states[random_indices], actions[random_indices], rewards[random_indices],
+             observations[random_indices], dones[random_indices])
+    return batch
 
 
 @jax.jit
@@ -106,11 +154,9 @@ def compute_q_targets(params: hk.Params, target_params: hk.Params,
     next_q_tm: ndarray = model.apply(target_params, observations)
     max_actions: ndarray = argmax(next_q, axis=1)
     targets: List = []
-    for index, action in enumerate(max_actions):
-        target_val: float = rewards[index] + \
-                            (1.0 - dones[index]) * (GAMMA * next_q_tm[index, action] - q[index, actions[index]])
-
-        q_target: ndarray = q[index] + target_val * one_hot(actions[index], env.action_space.n)
+    for index, (max_action, action, done) in enumerate(zip(max_actions, actions, dones)):
+        target_val: float = rewards[index] + (1.0 - done) * (GAMMA * next_q_tm[index, max_action] - q[index, action])
+        q_target: ndarray = q[index] + target_val * one_hot(action, env.action_space.n)
         targets.append(q_target)
     targets: jnp.ndarray = jnp.array(targets)
     return targets
@@ -122,6 +168,13 @@ def compute_action(params: hk.Params, state: ndarray) -> ndarray:
     return action
 
 
+def preprocessing(states: ndarray, actions: ndarray, rewards: ndarray, observations: ndarray,
+                  dones: ndarray) -> Tuple[jnp.ndarray, ndarray, ndarray, ndarray, ndarray]:
+    states: jnp.ndarray = jax.numpy.asarray(states)
+    dones = dones.astype(float64)
+    return states, actions, rewards, observations, dones
+
+
 class Agent:
     _replay_buffer: ReplayBuffer
     _params: hk.Params
@@ -130,8 +183,7 @@ class Agent:
     _episode_rewards: List[float]
 
     def __init__(self, params: hk.Params, opt_state: Mapping):
-        buffer_size: int = 100000
-        self._replay_buffer = ReplayBuffer(buffer_size=buffer_size, obs_shape=(buffer_size, 9))
+        self._replay_buffer = ReplayBuffer(buffer_size=BUFFER_SIZE, obs_shape=(BUFFER_SIZE, 9))
         self._params = params
         self._opt_state = opt_state
         self._target_params = params
@@ -158,16 +210,6 @@ class Agent:
     def _update_target_model(self):
         self._target_params = self._params
 
-    def save(self):
-        if not os.path.exists("lunar_lander"):
-            os.mkdir("lunar_lander")
-        with open("lunar_lander/params.pickle", "wb") as file:
-            dump(self._params, file)
-
-    def load(self):
-        with open("lunar_lander/params.pickle", "rb") as file:
-            self._params = load(file)
-
     def training(self):
         step_count: int = 0
         for episode in range(MAX_EPISODES):
@@ -183,7 +225,6 @@ class Agent:
                 observation, reward, done, info = env.step(action)
                 observation = append(observation, fraction_finished)
                 observation: ndarray = observation[newaxis, ...]
-                # env.render()
 
                 if step == MAX_STEPS:
                     done: bool = True
@@ -193,13 +234,29 @@ class Agent:
                 episode_reward += reward
 
                 if self._replay_buffer.size >= TRAINING_START and step_count % TRAIN_FREQUENCY == 0:
-                    states, actions, rewards, observations, dones = self._replay_buffer.sample_batch(BATCH_SIZE)
-                    states: jnp.ndarray = jax.numpy.asarray(states)
-                    dones = dones.astype(float64)
-                    q_targets: jnp.ndarray = compute_q_targets(self._params, self._target_params, states,
-                                                               actions, rewards, observations, dones)
-                    self._params, self._opt_state = train_step(self._params, self._opt_state,
-                                                               states, q_targets)
+                    states, actions, rewards, observations, dones = sample_batch(self._replay_buffer.size,
+                                                                                 self._replay_buffer.states,
+                                                                                 self._replay_buffer.actions,
+                                                                                 self._replay_buffer.rewards,
+                                                                                 self._replay_buffer.observations,
+                                                                                 self._replay_buffer.dones,
+                                                                                 BATCH_SIZE)
+                    states, actions, rewards, observations, dones = preprocessing(states,
+                                                                                  actions,
+                                                                                  rewards,
+                                                                                  observations,
+                                                                                  dones)
+                    q_targets: jnp.ndarray = compute_q_targets(self._params,
+                                                               self._target_params,
+                                                               states,
+                                                               actions,
+                                                               rewards,
+                                                               observations,
+                                                               dones)
+                    self._params, self._opt_state = train_step(self._params,
+                                                               self._opt_state,
+                                                               states,
+                                                               q_targets)
 
                 if done:
                     break
@@ -208,27 +265,57 @@ class Agent:
                 self._update_target_model()
 
             if episode % BACKUP_FREQUENCY == 0:
-                self.save()
+                save_training_state(self._params, self._opt_state)
 
             self._update_epsilon()
             self._update_episode_rewards(episode_reward)
-            print("Episode: {} -- Reward: {} -- Average: {}".
-                  format(episode, episode_reward, self._average_reward()))
 
             if self._average_reward() > 240:
-                self.save()
+                save_training_state(self._params, self._opt_state)
                 return
 
             end: float = time.time()
-            print('Time: {}s'.format(end - start))
+            if episode % 10 == 0:
+                print("Episode: {} -- Reward: {} -- Average: {}".
+                      format(episode, episode_reward, self._average_reward()))
+                print('Time: {}s'.format(end - start))
+
+
+# TODO: implement optimizer save and load
+def save_training_state(params: hk.Params, opt_state: Mapping):
+    if not os.path.exists("lunar_lander"):
+        os.mkdir("lunar_lander")
+    with open("lunar_lander/params.pickle", "wb") as file:
+        dump(params, file)
+    with open("lunar_lander/opt_state.pickle", "wb") as file:
+        dump(opt_state, file)
+
+
+def load_parameters() -> Tuple[hk.Params, Mapping]:
+    with open("lunar_lander/params.pickle", "rb") as file:
+        params: hk.Params = load(file)
+    with open("lunar_lander/opt_state.pickle", "rb") as file:
+        opt_state: Mapping = load(file)
+    return params, opt_state
+
+
+def visualize_agent():
+    state: ndarray = env.reset()
+    for step in range(MAX_STEPS):
+        fraction_finished: float = step / MAX_STEPS
+        state = append(state, fraction_finished)[newaxis, ...]
+        action: int = int(argmax(model.apply(parameters, state)))
+        state, reward, done, info = env.step(action)
+        env.render()
 
 
 if __name__ == '__main__':
     BATCH_SIZE: int = 64
+    BUFFER_SIZE: int = 100000
     MAX_STEPS: int = 1000
-    MAX_EPISODES: int = 10000
+    MAX_EPISODES: int = 20
     REPLACE_FREQUENCY: int = 50
-    BACKUP_FREQUENCY: int = 50
+    BACKUP_FREQUENCY: int = 20
     TRAINING_START: int = 256
     TRAIN_FREQUENCY: int = 4
     EPSILON: float = 1.0
