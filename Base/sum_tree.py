d7dec164875dc4c9edde4139a8286b8700740b9e
BuildTools <Tobias-Hettler@protonmail.com>
2022-09-02 21:53:45 +0200

    [base] fixed bug in sum tree

--- Base/sum_tree.py
+++ Base/sum_tree.py
@@ -1,43 +1,36 @@
 import numpy
-from numpy import ndarray
+from numpy import ndarray, vectorize
 import math
 
 
 def propagate_changes(tree: ndarray, node: int, change: float):
-    size = tree.size / 2
-    node += size - 1
-    while node > 0:
+    size = int(tree.size / 2)
+    node += size
+    while node >= 1:
         tree[node] += change
         node = math.floor(node / 2)
 
 
 def add(tree: ndarray, node: int, new_value: float):
-    size = tree.size / 2
-    change = new_value - tree[node + size - 1]
-    tree[node + size - 1] = new_value
+    size = int(tree.size / 2)
+    change = new_value - tree[node + size]
     propagate_changes(tree, node, change)
 
 
-def retrieve(tree: ndarray, value: float) -> int:
-    i = 0
-    size = tree.size / 2
-    while 2*i+2 <= size:
-        if tree[2*i+1] >= value or tree[2*i+2] == 0:
-            i = 2*i+1
-        else:
-            value -= tree[2*i+1]
-            i = 2*i+2
+def retrieve(tree: ndarray, values: ndarray) -> ndarray:
+    def ret(value: float):
+        i = 1
+        size = int(tree.size / 2)
+        while i+1 <= size:
+            if tree[2*i] >= value or tree[2*i+1] == 0:
+                i = 2*i
+            else:
+                value -= tree[2*i]
+                i = 2*i+1
+        return i - size
 
-    return i - size + 1
+    return vectorize(ret)(values)
 
 
 def gen_tree(buffer_size: int) -> ndarray:
     return numpy.zeros((2*buffer_size,))
-
-
-
-
-
-
-
-
